c2dd7fe9044b575af8d17c30ecb183897f763478
ZaraKnYu <razenxizu@gmail.com>
2026-02-15 21:02:38 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,7 @@ CAMERA_PACKAGE_NAME := com.android.camera
 
 BUILD_BROKEN_DUP_RULES := true
 
-$(call soong_config_set,camera,override_format_from_reserved,$(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED))
+$(call soong_config_set_bool,camera,override_format_from_reserved,$(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED))
 
 TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT = true
 TARGET_INCLUDES_MIUI_CAMERA := true
